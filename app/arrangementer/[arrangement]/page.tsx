2465dc1d980e4a6a6f7ea0205421110139583871
Allan Tolnæs <allan@Allans-MacBook-Air.local>
2025-07-02 20:47:56 +0200

    Changed params

--- app/arrangementer/[arrangement]/page.tsx
+++ app/arrangementer/[arrangement]/page.tsx
@@ -9,31 +9,31 @@ type Props = {
 };
 
 export default function ArrangementPage({ params }: Props) {
-  const { arrangement: arrangementLink } = params;
+  const { arrangement } = params;
 
-  const arrangement = arrangementer.find(
-    (item) => item.link === arrangementLink
+  const foundArrangement = arrangementer.find(
+    (item) => item.link === arrangement
   );
 
-  if (!arrangement) return notFound();
+  if (!foundArrangement) return notFound();
 
   return (
     <div className="w-full pt-[4vh] md:pt-[12vh] min-h-screen bg-basic overflow-hidden relative">
       <div className="flex flex-col justify-center w-4/5 mx-auto">
         <h1 className="text-bg text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-bold md:leading-[3.5rem] lg:leading-[4rem] xl:leading-[4.5rem] text-white mt-12 md:mt-0 py-12">
-          {arrangement.title}
+          {foundArrangement.title}
         </h1>
 
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
           <div>
             <p className="text-gray-900 text-lg mb-8" style={{ maxWidth: 550 }}>
-              {arrangement.description1}
+              {foundArrangement.description1}
             </p>
             <p className="text-gray-900 text-lg mb-8" style={{ maxWidth: 550 }}>
-              {arrangement.description2}
+              {foundArrangement.description2}
             </p>
             <p className="text-2xl font-bold mb-8 text-gray-900">
-              {arrangement.dato}
+              {foundArrangement.dato}
             </p>
             <div className="flex space-x-6 mb-12" />
           </div>
@@ -42,8 +42,8 @@ export default function ArrangementPage({ params }: Props) {
             style={{ maxWidth: 700 }}
           >
             <Image
-              src={arrangement.image}
-              alt={arrangement.title}
+              src={foundArrangement.image}
+              alt={foundArrangement.title}
               width={700}
               height={700}
               className="object-cover"
